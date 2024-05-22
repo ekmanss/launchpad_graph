a1d3eb3206458b32cb105a180ce6129e8944d9d4
ekmanss <hfers@msn.com>
2024-05-22 18:54:47 +0800

    main

--- config.ts
+++ config.ts
@@ -1,2 +1,7 @@
-export const Launchpad_address = "0xa5799672484ba24c1614a9adc6c35c54edbe0900"
-export const WETH = "0x4200000000000000000000000000000000000023"
+// blast_test
+// export const Launchpad_address = "0xa5799672484ba24c1614a9adc6c35c54edbe0900"
+// export const WETH = "0x4200000000000000000000000000000000000023"
+
+// blast_main
+export const Launchpad_address = "0x659a1d0b7cf7bc5021a42be436e42209965c6941"
+export const WETH = "0x4300000000000000000000000000000000000004"
